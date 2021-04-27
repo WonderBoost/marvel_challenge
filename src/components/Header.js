7e1a223057d1ee234c0f331d2c79af2316fa3a46
jsanchez <josegsv28@gmail.com>
2021-04-27 00:26:58 -0400

    structure & assets

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/marvel-logo.png'
+
+export const Header = () => {
+    return (
+        <div>
+            <header className="center">
+                
+            </header>
+        </div>
+    )
+}
